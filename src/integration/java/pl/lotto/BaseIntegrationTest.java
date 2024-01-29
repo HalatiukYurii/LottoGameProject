0aef85996364f661ea12d35499a0388a6af8ba80
HalatiukYurii <yuriy.galatyuk@gmail.com>
2024-01-30 00:17:53 +0100

    base integration tests and integration package

---
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -0,0 +1,8 @@
+package pl.lotto;
+
+import halatiukyurii.pl.lotto.domaine.LottoSpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LottoSpringBootApplication.class)
+public class BaseIntegrationTest {
+}
