0a6b7ecb12f2f7c1c6fe5ac8c6fc98de183b3b4a
HalatiukYurii <yuriy.galatyuk@gmail.com>
2024-03-11 20:50:36 +0100

    added awaitility, mocked repositories, added properties and scheduler

--- src/main/java/halatiukyurii/pl/lotto/LottoSpringBootApplication.java
+++ src/main/java/halatiukyurii/pl/lotto/LottoSpringBootApplication.java
@@ -4,9 +4,11 @@ import halatiukyurii.pl.lotto.domaine.numbergenerator.WinningNumbersGeneratorFac
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({WinningNumbersGeneratorFacadeConfigurationProperties.class})
+@EnableScheduling
 public class LottoSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoSpringBootApplication.class, args);
